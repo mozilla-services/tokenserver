f004c7de317b5704d36939d2e93c5c2924c9b213
Tarek Ziade <tarek@ziade.org>
2012-02-13 18:13:33 +0100

    added headers

--- tokenserver/__init__.py
+++ tokenserver/__init__.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 from repoze.who.plugins.vepauth.tokenmanager import SignedTokenManager
 from pyramid.threadlocal import get_current_registry
 from mozsvc.config import get_configurator
