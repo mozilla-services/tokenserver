eee09f6cb8e50fa0cc3b2fed8d8a3b44df7ced61
Tarek Ziade <tarek@ziade.org>
2012-02-13 18:09:42 +0100

    moved the token manager to the top nanmespace

--- tokenserver/__init__.py
+++ tokenserver/__init__.py
@@ -1,4 +1,22 @@
+from repoze.who.plugins.vepauth.tokenmanager import SignedTokenManager
+from pyramid.threadlocal import get_current_registry
 from mozsvc.config import get_configurator
+
+
+class NodeTokenManager(SignedTokenManager):
+    def __init__(self, *args, **kw):
+        super(NodeTokenManager, self).__init__(*args, **kw)
+        self.sentry = -1
+
+    def make_token(self, request, data):
+        if self.sentry is -1:
+            settings = get_current_registry().settings
+            self.sentry = settings.get('token.service_entry')
+
+        extra = {'service_entry': self.sentry}
+        token, secret, __ = super(NodeTokenManager, self).make_token(request,
+                                                                     data)
+        return token, secret, extra
 
 
 def includeme(config):
--- tokenserver/views.py
+++ tokenserver/views.py
@@ -1,7 +1,4 @@
-from pyramid.threadlocal import get_current_registry
 from cornice import Service
-from repoze.who.plugins.vepauth.tokenmanager import SignedTokenManager
-
 
 discovery = Service(name='discovery', path='/')
 
@@ -9,19 +6,3 @@ discovery = Service(name='discovery', path='/')
 @discovery.get()
 def _discovery(request):
     return {'sync': '1.0'}
-
-
-class NodeTokenManager(SignedTokenManager):
-    def __init__(self, *args, **kw):
-        super(NodeTokenManager, self).__init__(*args, **kw)
-        self.sentry = -1
-
-    def make_token(self, request, data):
-        if self.sentry is -1:
-            settings = get_current_registry().settings
-            self.sentry = settings.get('token.service_entry')
-
-        extra = {'service_entry': self.sentry}
-        token, secret, __ = super(NodeTokenManager, self).make_token(request,
-                                                                     data)
-        return token, secret, extra
