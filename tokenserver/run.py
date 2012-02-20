ddd9698aa98ab09b298177d8c4948f81f899c2db
Tarek Ziade <tarek@ziade.org>
2012-02-20 17:37:51 +0100

    added a bootstrap file for gunicorn

---
+++ tokenserver/run.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Runs the Application. This script can be called by any wsgi runner that looks
+for an 'application' variable
+"""
+import os
+from logging.config import fileConfig
+from ConfigParser import NoSectionError
+
+# setting up the egg cache to a place where apache can write
+os.environ['PYTHON_EGG_CACHE'] = '/tmp/python-eggs'
+
+# setting up logging
+ini_file = '/var/services/tokenserver/etc/tokenserver-dev.ini'
+ini_file = os.environ.get('TOKEN_INI', ini_file)
+try:
+    fileConfig(ini_file)
+except NoSectionError:
+    pass
+
+# running the app using Paste
+from paste.deploy import loadapp
+application = loadapp('config:%s' % ini_file)
