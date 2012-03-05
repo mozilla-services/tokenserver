7f0052fda75d5a1118b6c8ed9d6d7f00741df6ae
Tarek Ziade <tarek@ziade.org>
2012-03-05 14:28:45 +0100

    added the sharded class

--- tokenserver/assignment/sqlnode.py
+++ tokenserver/assignment/sqlnode.py
@@ -1,5 +1,6 @@
 """ SQL Mappers
 """
+import json
 from zope.interface import implements
 from tokenserver.assignment import INodeAssignment
 
@@ -16,4 +17,40 @@ class SQLNodeAssignment(SQLMetadata):
 
 
 class ShardedSQLNodeAssignment(ShardedSQLMetadata):
+    """Like the SQL backend, but with one DB per service
+    """
     implements(INodeAssignment)
+
+
+class SecuredShardedSQLNodeAssignment(ShardedSQLMetadata):
+    """Like the sharded backend, but proxies all writes to stoken
+    """
+    implements(INodeAssignment)
+
+    def __init__(self, proxy_uri, databases, create_tables, **kw):
+        base = super(SecuredShardedSQLNodeAssignment, self)
+        base.__init__(databases, create_tables, **kw)
+        self.proxy_uri = proxy_uri
+
+    def _proxy(self, method, url, data=None, headers=None):
+        if data is not None:
+            data = json.dumps(data)
+        status, headers, body = get_url(url, method, data, headers)
+        if body:
+            try:
+                body = json.loads(body)
+            except ValueError:
+                logger.error("bad json body from sreg (%s): %s" %
+                                                        (url, body))
+                raise BakenckError('Bad answer from proxy')
+        return status, body
+
+    def allocate_node(self, email, service):
+        """Calls the proxy to get an allocation"""
+        url = '%s/1.0/%s' % (self.proxy_uri, service)
+        data = json.dumps({'email': email})
+        status, body = self._proxy('POST', url, data)
+        if status != 200:
+            raise BackendError('Could not get an allocation')
+
+        return data['uid'], data['node']
