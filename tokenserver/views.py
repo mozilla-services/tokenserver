56401448a09723677331b736b3a76c1eab976881
Alexis Metaireau <alexis@notmyidea.org>
2012-03-09 18:39:02 +0100

    Change the way cornice services are defined.
    There is no need to use the @resource notation here, so let's get rid of it.

--- tokenserver/views.py
+++ tokenserver/views.py
@@ -1,19 +1,20 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import os
 import json
 
 from cornice import Service
-from cornice.resource import resource, view
+from vep.errors import Error as BrowserIDError
 
 from tokenserver.util import JsonError
 from tokenserver.verifiers import get_verifier
 
-#
-# Discovery page
-#
+# A GET on / returns the discovery API
+
 discovery = Service(name='discovery', path='/')
+token = Service(name='token', path='/1.0/{application}/{version}')
 
 
 @discovery.get()
@@ -24,24 +25,38 @@ def _discovery(request):
         return json.loads(f.read())
 
 
-#
-# token service
-#
+# validators
 def valid_assertion(request):
+    """Validate that the assertion given in the request is correct.
+
+    If not, add errors in the response so that the client can know what
+    happened.
+    """
+    def _raise_unauthorized():
+        raise JsonError(401, description='Unauthorized')
+
     token = request.headers.get('Authorization')
     if token is None:
-        raise JsonError(401, description='Unauthorized')
+        _raise_unauthorized()
+
     token = token.split()
     if len(token) != 2:
-        raise JsonError(401, description='Unauthorized')
+        _raise_unauthorized()
+
     name, assertion = token
     if name.lower() != 'browser-id':
         resp = JsonError(401, description='Unsupported')
         resp.www_authenticate = ('Browser-ID', {})
         raise resp
 
-    verifier = get_verifier()
-    verifier.verify(assertion)
+    try:
+        verifier = get_verifier()
+        verifier.verify(assertion)
+    except BrowserIDError:
+        _raise_unauthorized()
+
+    # everything sounds good, add the assertion to the list of validated fields
+    # and continue
 
     request.validated['assertion'] = assertion
 
@@ -66,20 +81,14 @@ def valid_app(request):
         request.validated['version'] = version
 
 
-@resource(path='/1.0/{application}/{version}')
-class TokenService(object):
-    def __init__(self, request):
-        self.request = request
+@token.get(validators=(valid_app, valid_assertion))
+def return_token(request):
 
-    @view(validators=(valid_app, valid_assertion))
-    def get(self):
-        request = self.request
+    # XXX here, build the token
+    assertion = request.validated['assertion']
+    application = request.validated['application']
+    version = request.validated['version']
+    #email = request.validated['email']
+    secrets = request.registry.settings['tokenserver.secrets_file']
 
-        # XXX here, build the token
-        assertion = request.validated['assertion']
-        application = request.validated['application']
-        version = request.validated['version']
-        #email = request.validated['email']
-        secrets = request.registry.settings['tokenserver.secrets_file']
-
-        return {'service_entry': 'http://example.com'}
+    return {'service_entry': 'http://example.com'}
