512984cbb8a8e93633ffb7b75b56582e1efeb5bc
Alexis Metaireau <alexis@notmyidea.org>
2012-03-16 13:28:59 +0100

    Add a dependency to PyVEP.

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
-import os
 from setuptools import setup, find_packages
 
 
-requires = ['cornice', 'mozsvc', 'powerhose', 'circus', 'wimms']
-
+requires = ['cornice', 'mozsvc', 'powerhose', 'circus', 'wimms', 'PyVEP']
 
 setup(name='tokenserver',
       version='0.1',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      entry_points = """\
+      entry_points="""\
       [paste.app_factory]
       main = tokenserver:main
       """,
