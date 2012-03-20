b3576f33ebc77c9794cb06c56a71074dadba79bb
Tarek Ziade <tarek@ziade.org>
2012-03-20 15:01:09 +0100

    make sure we raise an error on bad args

--- tokenserver/crypto/workers.py
+++ tokenserver/crypto/workers.py
@@ -1,6 +1,8 @@
 from powerhose.client.workers import Workers
 import sys
 
+if len(sys.argv) != 2:
+    raise Exception('Bad number of args for workers - %s' % str(sys.argv))
 
 workers = Workers(sys.argv[1])
 try:
