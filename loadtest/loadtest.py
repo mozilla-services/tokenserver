31fb1724f1ff71ce0f75fc071df54c30c0e9a0d5
Alexis Metaireau <alexis@notmyidea.org>
2012-04-05 02:48:27 +0200

    split the assertions generation from their use.
    this is useful for loadtesting, for instance.

--- loadtest/loadtest.py
+++ loadtest/loadtest.py
@@ -1,14 +1,14 @@
 from funkload.FunkLoadTestCase import FunkLoadTestCase
 
-from assertions import VALID, WRONG_ISSUER, WRONG_EMAIL_HOST
+from assertions import (VALID_ASSERTION, WRONG_ISSUER_ASSERTION,
+                        WRONG_EMAIL_HOST_ASSERTION, EXPIRED_TOKEN)
 
-DEFAULT_EMAIL = 'alexis@loadtest.localdomain'
 
-
-class SimpleTest(FunkLoadTestCase):
+class NodeAssignmentTest(FunkLoadTestCase):
 
     def setUp(self):
         self.root = self.conf_get('main', 'url')
+        self.token_exchange = '/1.0/aitc/1.0'
 
     def _do_token_exchange(self, assertion=None, status=200):
         self.setHeader('Authorization', 'Browser-ID %s' % assertion)
@@ -19,14 +19,12 @@ class SimpleTest(FunkLoadTestCase):
     def test_token_exchange(self):
         # a valid browserid assertion should be taken by the server and turned
         # back into an authentication token which is valid for 30 minutes.
-        self._do_token_exchange(VALID, 200)
+        self._do_token_exchange(VALID_ASSERTION, 200)
 
     def test_bad_assertions(self):
-        # a wrong certificate should return a 400 error
-        self._do_token_exchange(WRONG_ISSUER, 400)
-
-        # a wrong issuer cert as well
-        self._do_token_exchange(WRONG_EMAIL_HOST, 200)
+        self._do_token_exchange(WRONG_EMAIL_HOST_ASSERTION, 401)
+        self._do_token_exchange(EXPIRED_TOKEN, 401)
+        self._do_token_exchange(WRONG_ISSUER_ASSERTION, 401)
 
 if __name__ == '__main__':
     import unittest
