abdbf9f3d031bfcfdfde0605ab5e9147eac93d6a
Tarek Ziade <tarek@ziade.org>
2012-04-19 13:01:48 +0200

    fixed path

--- ezpylibmc.sh
+++ ezpylibmc.sh
@@ -1,6 +1,6 @@
 if [ -e "/opt/local/include/libmemcached/memcached.h" ]
 then
-    LIBMEMCACHED= bin/pip install pylibmc==1.2.2
+    LIBMEMCACHED=/opt/local/ bin/pip install pylibmc==1.2.2
 else
     bin/pip install pylibmc==1.2.2
 fi
