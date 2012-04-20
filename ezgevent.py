81c48c1861dc617e5537f5562e3c7c546f93a4e6
Alexis Metaireau <alexis@notmyidea.org>
2012-04-20 13:19:03 +0200

    Use people.mozilla.com rather than tarek/alexis boxes

--- ezgevent.py
+++ ezgevent.py
@@ -5,15 +5,16 @@ major = '2'
 minor = sys.version_info[1]
 plat = sys.platform
 
+HOST = "http://people.mozilla.com/~ametaireau/eggs/"
 
 releases = {
-  (False, 6, 'darwin'): 'http://ziade.org/gevent-0.13.7-py2.6-macosx-10.7-intel.egg',
-  (True, 6, 'darwin'): 'http://ziade.org/gevent-0.13.7-py2.6-macosx-10.7-intel.egg',
-  (False, 7, 'darwin'): 'http://ziade.org/gevent-0.13.7-py2.7-macosx-10.7-intel.egg',
-  (True, 7, 'darwin'): 'http://ziade.org/gevent-0.13.7-py2.7-macosx-10.7-intel.egg',
+  (False, 6, 'darwin'): 'gevent-0.13.7-py2.6-macosx-10.7-intel.egg',
+  (True, 6, 'darwin'): 'gevent-0.13.7-py2.6-macosx-10.7-intel.egg',
+  (False, 7, 'darwin'): 'gevent-0.13.7-py2.7-macosx-10.7-intel.egg',
+  (True, 7, 'darwin'): 'gevent-0.13.7-py2.7-macosx-10.7-intel.egg',
 }
 
 try:
-    print releases[is_64bits, minor, plat]
+    print HOST + releases[is_64bits, minor, plat]
 except KeyError:
     print 'http://pypi.python.org/packages/source/g/gevent/gevent-0.13.7.tar.gz'
