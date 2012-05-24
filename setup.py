a539a5c940e651f9453932382897fa6f773fbc48
Tarek Ziade <tarek@ziade.org>
2012-05-24 15:59:46 +0200

    starting 0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['cornice', 'mozsvc', 'powerhose', 'circus', 'wimms', 'PyBrowserID',
             'pylibmc', 'metlog-py']
 
 setup(name='tokenserver',
-      version='0.2',
+      version='0.3',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
