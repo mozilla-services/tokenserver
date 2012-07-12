caffdb0079fb2e4ec835efe1b7b683e76fdd81a7
Pete Fritchman <petef@databits.net>
2012-07-12 10:24:14 -0700

    add a flag to test stoken

--- loadtest/workers.sh
+++ loadtest/workers.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# workers.sh [-n] worker_count
+#
+# spawns $worker_count loadtest jobs
+# if -n is specified, will create new users (that hit stoken)
+
+TEST="test_single_token_exchange"
+if [ "$1" = "-n" ]; then
+    TEST="test_single_token_exchange_new_user"
+    shift
+fi
 
 workers=${1:-5}
 tmpdir=$(mktemp -d)
@@ -10,5 +20,5 @@ xapply -xP$workers '
     mkdir -p %1
     cp loadtest.py NodeAssignmentTest.conf %1
     cd %1
-    ../../bin/fl-run-bench -f loadtest.py NodeAssignmentTest.test_single_token_exchange
+    ../../bin/fl-run-bench -f loadtest.py NodeAssignmentTest.'"$TEST"'
 ' $(seq 1 $workers)
