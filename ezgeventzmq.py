712fa00f75b5ffc8fdcacaf3fcbcbc4dcf844ae5
Tarek Ziade <tarek@ziade.org>
2012-07-24 17:47:47 +0200

    fixed typo

--- ezgeventzmq.py
+++ ezgeventzmq.py
@@ -8,8 +8,8 @@ plat = sys.platform
 HOST = "http://people.mozilla.com/~ametaireau/eggs/"
 
 releases = {
-  (False, 6, 'darwin'): 'gevent-0.13.7-py2.6-macosx-10.7-intel.egg',
-  (True, 6, 'darwin'): 'gevent-0.13.7-py2.6-macosx-10.7-intel.egg',
+  (False, 6, 'darwin'): 'gevent_zeromq-0.2.0-py2.6-macosx-10.7-intel.egg',
+  (True, 6, 'darwin'): 'gevent_zeromq-0.2.0-py2.6-macosx-10.7-intel.egg',
   (False, 7, 'darwin'): 'gevent_zeromq-0.2.0-py2.7-macosx-10.7-intel.egg',
   (True, 7, 'darwin'): 'gevent_zeromq-0.2.0-py2.7-macosx-10.7-intel.egg',
   (False, 6, 'linux2'): 'gevent_zeromq-0.2.0-py2.6.egg',
