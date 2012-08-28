cca58cb695d71458909dc2c0fca0f2becdf4ab6e
Alexis Metaireau <alexis@notmyidea.org>
2012-08-28 10:58:56 +0200

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['cornice', 'mozsvc', 'powerhose', 'circus', 'wimms', 'PyBrowserID',
             'pylibmc', 'metlog-py']
 
 setup(name='tokenserver',
-        version='1.1',
+        version='1.2',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
