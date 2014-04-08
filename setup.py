56a520fce1f60daaa0bc3cda33c1d32c8524865b
Ryan Kelly <rkelly@mozilla.com>
2014-04-09 03:20:52 +1000

    Add third place in version number, why not

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['cornice', 'mozsvc', 'powerhose', 'circus', 'wimms', 'PyBrowserID',
             'metlog-py']
 
 setup(name='tokenserver',
-        version='1.2',
+        version='1.2.0',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
