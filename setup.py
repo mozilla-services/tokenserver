fae6ad55f046b64c2f2d6ae21dfc2deb301375c0
Ryan Kelly <rkelly@mozilla.com>
2014-04-19 13:28:14 +1000

    Add protobuf to setup.py dependencies list

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 requires = ['cornice', 'mozsvc', 'powerhose', 'circus', 'wimms', 'PyBrowserID',
-            'metlog-py']
+            'metlog-py', 'protobuf']
 
 setup(name='tokenserver',
         version='1.2.2',
