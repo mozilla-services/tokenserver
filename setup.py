454089c96cd182e05a90c4d41fbeb4ef34e55176
Ryan Kelly <rkelly@mozilla.com>
2014-05-23 12:18:55 +1000

    Bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['cornice', 'mozsvc>=0.8', 'PyBrowserID', 'testfixtures']
             
 
 setup(name='tokenserver',
-        version='1.2.6',
+        version='1.2.7',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
