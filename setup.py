7949c59a8f14f0640b4ba76390ca7ef90757a793
Ryan Kelly <rfkelly@mozilla.com>
2016-02-12 08:18:25 +1100

    Prep v1.2.18

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['cornice', 'mozsvc>=0.8', 'PyBrowserID', 'testfixtures']
             
 
 setup(name='tokenserver',
-        version='1.2.17',
+        version='1.2.18',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
