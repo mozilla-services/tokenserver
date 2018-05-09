2ade4a7c6ab8658a11a7674c47ea74afdc7ad2bb
Ryan Kelly <rfkelly@mozilla.com>
2018-05-10 09:23:17 +1000

    Prep v1.3.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 
 setup(name='tokenserver',
-      version='1.3.0',
+      version='1.3.1',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
