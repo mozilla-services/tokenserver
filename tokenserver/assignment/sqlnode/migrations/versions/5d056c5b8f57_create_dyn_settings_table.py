459b859da7d249d289e94b4921c14002bd4849ff
jrconlin <jr+git@mozilla.com>
2020-01-06 09:49:44 -0800

    f flake8

--- tokenserver/assignment/sqlnode/migrations/versions/5d056c5b8f57_create_dyn_settings_table.py
+++ tokenserver/assignment/sqlnode/migrations/versions/5d056c5b8f57_create_dyn_settings_table.py
@@ -5,13 +5,12 @@ Revises: 75e8ca84b0bc
 Create Date: 2020-01-06 08:16:15.546054
 
 """
+from alembic import op
+import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '5d056c5b8f57'
 down_revision = '75e8ca84b0bc'
-
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
