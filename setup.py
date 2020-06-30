f5b939718ec38e3a17af42095310924248f8c065
jrconlin <jr+git@mozilla.com>
2020-06-30 23:38:26 +0000

    chore: tag 1.5.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = load_req("dev-requirements.txt")
 
 
 setup(name='tokenserver',
-      version='1.5.6',
+      version='1.5.7',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
